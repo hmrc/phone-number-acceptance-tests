5ea3c0a4528e25a38ad6a3205f4ca42f8580d84f
mar36tin <mar36tin@gmail.com>
2022-08-04 12:23:57 +0100

    Validation acceptance tests happy and unhappy path

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,20 @@
 ENV=${1:-local}
 
 # Scalafmt checks have been separated from the test command to avoid OutOfMemoryError in Jenkins
-sbt scalafmtCheckAll scalafmtSbtCheck
+#sbt scalafmtCheckAll scalafmtSbtCheck
+
+echo "======== cip phone number api acceptance tests ========"
+if [[ $ENV="local" ]]
+then
+  echo "Before you run tests ensure all cip phone number services are running"
+  echo "Are all CIP PHONE NUMBER services running?"
+  select yn in "Yes" "No"; do
+      case $yn in
+          Yes ) break;;
+          No ) exit;;
+      esac
+  done
+fi
+echo "=================================================================="
 
 sbt -Denvironment=$ENV "testOnly uk.gov.hmrc.test.api.specs.*"
