501f380559b0f2033da8a125df1023c11ccb4062
berlin-hmrc <berlin.bernadin1@digital.hmrc.gov.uk>
2023-02-21 17:33:11 +0000

    Dependancies update to match Scala version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "org.scalatest"       %% "scalatest"               % "3.2.0"   % Test,
-    "com.vladsch.flexmark" % "flexmark-all"            % "0.35.10" % Test,
+    "org.scalatest"       %% "scalatest"               % "3.2.13"  % Test,
+    "com.vladsch.flexmark" % "flexmark-all"            % "0.62.2"  % Test,
     "com.typesafe"         % "config"                  % "1.3.2"   % Test,
     "com.typesafe.play"   %% "play-ahc-ws-standalone"  % "2.1.2"   % Test,
     "com.typesafe.play"   %% "play-ws-standalone-json" % "2.1.2"   % Test,
