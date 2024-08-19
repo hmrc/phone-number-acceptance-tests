3cd6ff06d944418190404a4114823f6ee91236b9
Saqib@Ee <saqib.saleem1@digital.hmrc.gov.uk>
2024-08-19 08:18:56 +0100

    CIR-1669: Tidy up dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,16 +1,19 @@
 import sbt._
 
 object Dependencies {
+  private val pekkoVersion = "1.0.3"
+  private val playVersion = "3.0.5"
+  private val scalatestVersion = "3.2.19"
 
   val test = Seq(
-    "org.scalatest" %% "scalatest" % "3.2.19" % Test,
+    "org.scalatest" %% "scalatest" % scalatestVersion % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % Test,
     "com.typesafe" % "config" % "1.4.3" % Test,
-    "org.apache.pekko" %% "pekko-actor-typed" % "1.0.3" % Test,
-    "org.apache.pekko" %% "pekko-serialization-jackson" % "1.0.3" % Test,
-    "org.apache.pekko" %% "pekko-slf4j" % "1.0.3" % Test,
-    "org.playframework" %% "play-ahc-ws-standalone" % "3.0.5" % Test,
-    "org.playframework" %% "play-ws-standalone-json" % "3.0.5" % Test,
+    "org.apache.pekko" %% "pekko-actor-typed" % pekkoVersion % Test,
+    "org.apache.pekko" %% "pekko-serialization-jackson" % pekkoVersion % Test,
+    "org.apache.pekko" %% "pekko-slf4j" % pekkoVersion % Test,
+    "org.playframework" %% "play-ahc-ws-standalone" % playVersion % Test,
+    "org.playframework" %% "play-ws-standalone-json" % playVersion % Test,
     "org.slf4j" % "slf4j-simple" % "2.0.13" % Test,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % "2.2.0" % Test
   )
