2d939f87852556f3807a35125d76244723664e8e
Saqib@Ee <saqib.saleem1@digital.hmrc.gov.uk>
2024-08-19 07:58:11 +0100

    CIR-1669: Bump dep versions for play3

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ lazy val testSuite = (project in file("."))
   .settings(
     name := "phone-number-acceptance-tests",
     version := "0.1.0",
-    scalaVersion := "2.13.10",
+    scalaVersion := "2.13.12",
     scalacOptions ++= Seq("-feature"),
     libraryDependencies ++= Dependencies.test
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,15 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "org.scalatest"       %% "scalatest"               % "3.2.15"  % Test,
-    "com.vladsch.flexmark" % "flexmark-all"            % "0.62.2"  % Test,
-    "com.typesafe"         % "config"                  % "1.4.2"   % Test,
-    "com.typesafe.play"   %% "play-ahc-ws-standalone"  % "2.1.10"   % Test,
-    "com.typesafe.play"   %% "play-ws-standalone-json" % "2.1.10"   % Test,
-    "org.slf4j"            % "slf4j-simple"            % "2.0.5"  % Test,
-    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-28" % "0.71.0"  % Test
+    "org.scalatest" %% "scalatest" % "3.2.19" % Test,
+    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % Test,
+    "com.typesafe" % "config" % "1.4.3" % Test,
+    "org.apache.pekko" %% "pekko-actor-typed" % "1.0.3" % Test,
+    "org.apache.pekko" %% "pekko-serialization-jackson" % "1.0.3" % Test,
+    "org.apache.pekko" %% "pekko-slf4j" % "1.0.3" % Test,
+    "org.playframework" %% "play-ahc-ws-standalone" % "3.0.5" % Test,
+    "org.playframework" %% "play-ws-standalone-json" % "3.0.5" % Test,
+    "org.slf4j" % "slf4j-simple" % "2.0.13" % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % "2.2.0" % Test
   )
 }
