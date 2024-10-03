89dbfa69ee62e7774be4b4acf9cf62c3a11779fd
Saqib@Ee <saqib.saleem1@digital.hmrc.gov.uk>
2024-10-03 10:18:25 +0100

    CIR-1723: Update to play3

--- build.sbt
+++ build.sbt
@@ -1,9 +1,10 @@
+ThisBuild / scalaVersion := "2.13.14"
+ThisBuild / majorVersion := 0
+
 lazy val testSuite = (project in file("."))
   .disablePlugins(JUnitXmlReportPlugin) //Required to prevent https://github.com/scalatest/scalatest/issues/1427
   .settings(
     name := "phone-number-acceptance-tests",
-    version := "0.1.0",
-    scalaVersion := "2.13.10",
     scalacOptions ++= Seq("-feature"),
     libraryDependencies ++= Dependencies.test
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,10 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "org.scalatest"       %% "scalatest"               % "3.2.15"  % Test,
-    "com.vladsch.flexmark" % "flexmark-all"            % "0.62.2"  % Test,
-    "com.typesafe"         % "config"                  % "1.4.2"   % Test,
-    "com.typesafe.play"   %% "play-ahc-ws-standalone"  % "2.1.10"   % Test,
-    "com.typesafe.play"   %% "play-ws-standalone-json" % "2.1.10"   % Test,
-    "org.slf4j"            % "slf4j-simple"            % "2.0.5"  % Test,
-    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-28" % "0.71.0"  % Test
+    "com.typesafe" % "config" % "1.4.3" % Test,
+    "com.typesafe.play" %% "play-ahc-ws-standalone" % "2.2.9" % Test,
+    "com.typesafe.play" %% "play-ws-standalone-json" % "2.2.9" % Test,
+    "org.slf4j" % "slf4j-simple" % "2.0.13" % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % "2.2.0" % Test
   )
 }
