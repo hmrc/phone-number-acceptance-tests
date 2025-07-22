a3f8a1efcb44b109d3011cfee681474cfcf18a0a
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2025-07-22 14:58:58 +0100

    CIR-2085 Uplift dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object Dependencies {
 
-  val test = Seq(
-    "com.typesafe" % "config" % "1.4.3" % Test,
-    "com.typesafe.play" %% "play-ahc-ws-standalone" % "2.2.9" % Test,
-    "com.typesafe.play" %% "play-ws-standalone-json" % "2.2.9" % Test,
-    "org.slf4j" % "slf4j-simple" % "2.0.13" % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % "2.2.0" % Test
+  val test: Seq[ModuleID] = Seq(
+    "com.typesafe"       % "config"                   % "1.4.4"   % Test,
+    "com.typesafe.play" %% "play-ahc-ws-standalone"   % "2.2.11"  % Test,
+    "com.typesafe.play" %% "play-ws-standalone-json"  % "2.2.11"  % Test,
+    "org.slf4j"          % "slf4j-simple"             % "2.0.17"  % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30"  % "2.7.0"   % Test
   )
 }
